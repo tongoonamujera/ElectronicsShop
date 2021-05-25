cfa32aad0ada58857c01fa4e528a2087433603a5
tongoonamujera <tongoonamujera@gmail.com>
2021-05-25 10:28:08 +0200

    added more styles

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :televisions
   resources :cellphones
   root "home#index"
+
+  get '/home/', to: 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
